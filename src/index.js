2e85ca27e3aae042e11cf83d3536ff4caf2fa545
ezra kipchirchir <ezrakipchirchir1034@gmail.com>
2022-06-26 12:32:48 +0300

    fetch api

--- src/index.js
+++ src/index.js
@@ -6,4 +6,22 @@ document.addEventListener('DOMContentLoaded', () => {
 	const beerDescription = document.querySelector("#beer-description")
 	const reviewList = document.getElementById("review-list")
 	beerNavList()
+    fetch("http://localhost:3000/beers")
+		.then((res) => res.json())
+		.then((beersData) => {
+			beerName.textContent = beersData[ 0 ].name
+			beerImage.src = beersData[ 0 ].image_url
+			beerDescription.textContent = beersData[ 0 ].description
+			
+			//display customer reviews
+			beersData[ 0 ].reviews.forEach(review => {
+				let reviews = document.createElement('li')
+				reviews.innerText = review
+				reviewList.appendChild(reviews)
+				reviews.addEventListener('click', (e) => {
+					e.preventDefault()
+					reviews.remove()
+				})
+			})
+        })
 })
