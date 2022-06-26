656d15874f26ee1fe7dc972f34b669324f5dd729
ezra kipchirchir <ezrakipchirchir1034@gmail.com>
2022-06-26 10:29:57 +0300

    initialize files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 // Code here
+document.addEventListener('DOMContentLoaded', () => {
+
+	const beerName = document.querySelector("#beer-name")
+	const beerImage = document.querySelector("#beer-image")
+	const beerDescription = document.querySelector("#beer-description")
+	const reviewList = document.getElementById("review-list")
+	beerNavList()
+})
