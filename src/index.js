5441639e437b0d641a3e3485139f6604a5492f6f
ezra kipchirchir <ezrakipchirchir1034@gmail.com>
2022-06-26 12:54:28 +0300

    display items on click

--- src/index.js
+++ src/index.js
@@ -68,3 +68,40 @@ reviewForm.addEventListener('submit',(e)=>{
 		})
     }
 
+
+
+    //displaying drinks on main menu on click via nav-list
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchData()
+      
+
+function fetchData() {
+   fetch("http://localhost:3000/beers")
+        .then(response => response.json())
+            .then(data => {
+                return renderBeers(data);
+            })
+    }     
+
+    fetchData();
+    function renderBeers(beers) {
+        beers.forEach(beer => {   
+        const beerList = document.getElementById("beer-list")
+        const flataBeer = document.createElement("li")
+        flataBeer.textContent = beer.name
+        beerList.appendChild(flataBeer)
+        flataBeer.addEventListener('click', () => {
+            const beer_image = document.getElementById("beer-image")
+            beer_image.src = beer.image_url
+            console.log(beer_image)
+            const heading = document.querySelector('h2')
+            heading.textContent = beer.name;
+            const describe = document.getElementById("beer-description")
+            describe.textContent = beer.description;
+            const review = document.querySelector("#review-list")
+            review.textContent = beer.reviews;
+        });
+      });
+    }
+})
