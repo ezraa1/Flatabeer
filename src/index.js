42d3336d58e2b756e5efe60072163cb797a34bfb
ezra kipchirchir <ezrakipchirchir1034@gmail.com>
2022-06-26 12:47:23 +0300

    create a navlist of beers

--- src/index.js
+++ src/index.js
@@ -43,3 +43,28 @@ reviewForm.addEventListener('submit',(e)=>{
 		reviewUpdate.remove()
 	})
 })
+
+
+
+  //See a menu of all beers in the `<nav>` 
+
+  function beerNavList() {
+	fetch("http://localhost:3000/beers")
+		.then((res) => res.json())
+       .then((beersData) => {
+			let data1 = "";
+			beersData.map((values) => {
+				data1 += ` <ul id="beer-list">
+				<li>${values.name}</li></ul>
+				<div class="beer-details">
+			
+				<p>
+				  <em id="beer-description">${values.description}</em>
+				</p>
+				<h3>${values.reviews}</h3> </div>
+				`
+			})
+            document.querySelector("nav").innerHTML = data1;
+		})
+    }
+
